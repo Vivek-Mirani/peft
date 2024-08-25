d23574feaf593c7df63c3c3c2b3887494d79e59c
GarimaBansal72 <103357469+GarimaBansal72@users.noreply.github.com>
2024-08-25 05:41:08 +0530

    Update layer.py

--- src/peft/tuners/lora/layer.py
+++ src/peft/tuners/lora/layer.py
@@ -126,7 +126,7 @@ class LoraLayer(BaseTunerLayer):
 
         # Register hooks to maintain sparsity during training
         self.lora_A[adapter_name].weight.register_hook(lambda grad: grad*mask_A)
-        self.lora_B[adapter_name].weight.register_hook(lambda grad: grad*mask_B.t())
+        self.lora_B[adapter_name].weight.register_hook(lambda grad: grad*mask_B)
         print("Masked A - ", self.lora_A[adapter_name].weight.data)
         print("Masked B - ", self.lora_B[adapter_name].weight.data)
 
