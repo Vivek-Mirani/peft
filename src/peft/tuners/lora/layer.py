Corrected sparsity formula

src/peft/tuners/lora/layer.py
@@ -566,7 +566,7 @@ class Linear(nn.Module, LoraLayer):
                 if not self.use_dora[active_adapter]:
                     masked_output = self.mask_W[active_adapter] * lora_B(lora_A(dropout(x)))
                     print(masked_output)
-                    print("Sparsity - ", torch.count_nonzero(masked_output)/masked_output.numel())
+                    print("Sparsity - ", 1-(torch.count_nonzero(masked_output).item()/masked_output.numel()))
                     result = result + masked_output * scaling
                 else:
                     x = dropout(x)